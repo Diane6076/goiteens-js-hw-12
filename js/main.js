285bd6475fe9354d74d34306318b2e58819f21a1
diana <dskripka005@gmail.com>
2023-09-04 18:49:17 +0300

    fixone

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//1
+const showMessage = () => {
+    alert('Hello World!');
+};
+const helloBtn = document.getElementById('helloBtn');
+helloBtn.addEventListener('click', showMessage);
+//2
+
+//3
+
+//4
+
+//5
