7dde0c9793ef1f4bddff33775bffff9e47157d28
diana <dskripka005@gmail.com>
2023-09-04 19:07:51 +0300

    fixthree

--- js/main.js
+++ js/main.js
@@ -7,7 +7,14 @@ helloBtn.addEventListener('click', showMessage);
 //2
 
 //3
+const clickCountElement = document.getElementById('clickCount');
 
+let clickCount = 0;
+
+document.addEventListener('click', () => {
+    clickCount++;
+    clickCountElement.textContent = clickCount.toString();
+});
 //4
 
 //5
