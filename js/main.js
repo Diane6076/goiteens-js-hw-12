75c83afc95d3691a9fd6542e2cf4acef1d71a4e3
diana <dskripka005@gmail.com>
2023-09-04 20:25:15 +0300

    fixtwo

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ const helloBtn = document.getElementById('helloBtn');
 helloBtn.addEventListener('click', showMessage);
 //2
 
+const p = document.querySelector('guessResult');
+let inp ;
+let randomNumber;
+
+const checkGuess = () => {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    inp = Number(document.querySelector('guessField').value);
+  if (inp < 1 || inp > 100) {
+     return p.textContent = 'u enter vrong num';
+  } else if (inp === randomNumber) {
+    return p.textContent = 'congradulation';
+  } else {
+    p.textContent = 'wrong!';
+  }
+}
+
+
 //3
 const clickCountElement = document.getElementById('clickCount');
 
